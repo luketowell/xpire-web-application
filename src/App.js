4e8eb6a47e68991c003ea74ac21489289afb26a9
Luke Towell <luke.towell@hotmail.com>
2020-08-15 20:56:28 +0100

    created all of the initial Pages
    -created all of the pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './css/style.css';
+import Home from './pages/home';
+import Login from './pages/login';
+import StoreInfo from './pages/storeInfo';
+import Metrics from './pages/metrics';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Home/>
+      <Login/>
+      <StoreInfo/>
+      <Metrics/>
     </div>
   );
 }
---
+++ src/pages/storeInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StoreInfo extends Component {
+    render(){
+        return (
+        <div>
+            <h2>Store Info</h2>
+        </div>
+        )
+    }
+}
+
+export default StoreInfo;
