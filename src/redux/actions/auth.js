312643df74fb1e5399fe1096d846025e06681675
Luke Towell <luke.towell@hotmail.com>
2020-08-16 21:56:30 +0100

    Implemented the get stores redux call
    - implemented the get stores redux which successfully gets all stores from the DB.

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,8 +1,50 @@
-import { USER_AUTH } from '../constants';
+import { USER_AUTH,
+    GET_STORES_PENDING,
+    GET_STORES_SUCCESS,
+    GET_STORES_FAILED } from '../actionTypes';
+import request from '../../utils/request';
 
 export const authUser = () => {
     console.log("auth user action")
     return {
         type: USER_AUTH
+    }
+}
+
+
+export const getStores = () => {
+    console.log("Retrieving stores from the database")
+    return (dispatch) => {
+        dispatch({
+            type:GET_STORES_PENDING
+        });
+
+        retrieveStores()
+        .then(response => {
+            dispatch({
+                type:GET_STORES_SUCCESS,
+                payload: response
+            })
+        })
+        .catch(error=>{
+            dispatch({
+                type:GET_STORES_FAILED,
+                payload: error
+            })
+        })
+
+    } 
+}
+
+// functions
+const retrieveStores = async () => {
+    let url = "/store/all"
+    let method = "GET"
+    try {
+        let response = await request(url, method, null)
+        return response
+    }
+    catch(error){
+        throw error
     }
 }
