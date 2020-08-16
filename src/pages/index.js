d532304decef43800ea23aded60efae656e41326
Luke Towell <luke.towell@hotmail.com>
2020-08-16 13:17:44 +0100

    set up pages index.js
    - Grouped all pages into an index page for easier imports in navigation setup

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './home';
+export { default as Login } from './login';
+export { default as Metrics } from './metrics';
+export { default as StoreInfo } from './storeInfo';
