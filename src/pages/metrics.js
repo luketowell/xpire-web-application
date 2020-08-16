ebbcc7060d925421176446724a244fdf7ee10d77
Luke Towell <luke.towell@hotmail.com>
2020-08-16 15:56:43 +0100

    Added the navigation links to pages
    - Added the home, metric and store info navigation bar.

--- src/pages/metrics.js
+++ src/pages/metrics.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import NavLinks from '../components/Navigation/NavLinks'
 
 class Metrics extends Component {
     render(){
         return (
         <div>
+            <NavLinks/>
             <h2>Metrics</h2>
         </div>
         )
--- src/pages/storeInfo.js
+++ src/pages/storeInfo.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import NavLinks from '../components/Navigation/NavLinks' 
 
 class StoreInfo extends Component {
     render(){
         return (
         <div>
+            <NavLinks/>
             <h2>Store Info</h2>
         </div>
         )
