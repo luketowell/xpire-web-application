6ffde7292e4754a3d44e3dd07701c50e75ca58b2
Luke Towell <luke.towell@hotmail.com>
2020-08-17 16:34:11 +0100

    Started the functionality of the categories redux
    - set up categories request action

--- src/redux/actions/item.js
+++ src/redux/actions/item.js
@@ -1,8 +1,42 @@
-import { USER_AUTH } from '../actionTypes';
+import { USER_AUTH, GET_CATEGORIES_PENDING, GET_CATEGORIES_SUCCESS, GET_CATEGORIES_FAILED } from '../actionTypes';
+import request from '../../utils/request';
 
 export const authUser = () => {
-    console.log("auth user action")
     return {
         type: USER_AUTH
+    }
+}
+
+export const getCategories = () => {
+    return (dispatch) => {
+        dispatch({
+            type:GET_CATEGORIES_PENDING
+        })
+        this.getCategoriesRequest()
+        .then((response) => {
+            dispatch({
+                type:GET_CATEGORIES_SUCCESS,
+                payload: response
+            })
+            
+        })
+        .catch((error) => {
+            dispatch({
+                type:GET_CATEGORIES_FAILED,
+                payload: error
+            })
+        })
+    }
+}
+
+const getCategoriesRequest = async () => {
+    let url = "/itemcategories/all"
+    let method = "POST"
+    try {
+        let response = await request(url, method, null)
+        return response
+    }
+    catch(error){
+        throw error
     }
 }
