1c931ef373e1a305924ff079c2e7049bf142996d
Luke Towell <luke.towell@hotmail.com>
2020-08-24 11:00:57 +0100

    updated redux for item
    - updated the redux for retrieving storeItemDetails
    - updated the reducer for retrieving store item

--- src/redux/actions/item.js
+++ src/redux/actions/item.js
@@ -1,4 +1,9 @@
-import { FIND_ITEM_PENDING } from '../actionTypes';
+import {
+    FIND_ITEM_PENDING,
+    GET_STORE_ITEMS_BY_ID_PENDING,
+    GET_STORE_ITEMS_BY_ID_SUCCESS,
+    GET_STORE_ITEMS_BY_ID_FAILURE,
+} from '../actionTypes';
 import request from '../../utils/request';
 import displayCurrentStore from '../../utils/currentStore';
 
@@ -6,5 +11,27 @@ export const findProductInformation = (ItemNumber) => {
     return {
         type: FIND_ITEM_PENDING,
         payload: ItemNumber,
+    };
+};
+
+export const getStoreItemById = (id) => {
+    console.log(id);
+    return (dispatch, getState) => {
+        dispatch({
+            type: GET_STORE_ITEMS_BY_ID_PENDING,
+        });
+        request(`/storeitem/id/${id}`, 'GET')
+            .then((response) => {
+                dispatch({
+                    type: GET_STORE_ITEMS_BY_ID_SUCCESS,
+                    payload: response,
+                });
+            })
+            .catch((error) => {
+                dispatch({
+                    type: GET_STORE_ITEMS_BY_ID_FAILURE,
+                    payload: error,
+                });
+            });
     };
 };
--- src/redux/reducers/item.js
+++ src/redux/reducers/item.js
@@ -1,11 +1,35 @@
-import { FIND_ITEM_PENDING } from '../actionTypes';
+import {
+    FIND_ITEM_PENDING,
+    GET_STORE_ITEMS_BY_ID_PENDING,
+    GET_STORE_ITEMS_BY_ID_SUCCESS,
+    GET_STORE_ITEMS_BY_ID_FAILURE,
+} from '../actionTypes';
 
 const initialState = {
     selectedItem: '',
+    storeItemStatus: '',
+    storeItemDetails: {},
 };
 
 const CategoryReducer = (state = initialState, action) => {
     switch (action.type) {
+        case GET_STORE_ITEMS_BY_ID_PENDING:
+            return {
+                ...state,
+                storeItemStatus: 'pending',
+            };
+        case GET_STORE_ITEMS_BY_ID_SUCCESS:
+            return {
+                ...state,
+                storeItemStatus: 'completed',
+                storeItemDetails: action.payload,
+            };
+        case GET_STORE_ITEMS_BY_ID_FAILURE:
+            return {
+                ...state,
+                storeItemStatus: 'failed',
+                error: action.payload.message,
+            };
         case FIND_ITEM_PENDING:
             return {
                 ...state,
